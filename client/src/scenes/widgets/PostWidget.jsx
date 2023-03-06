1ca8aedd906e7c702e4036e0bad153e0bfb574a7
om dev <omdev74@gmail.com>
2023-03-06 08:28:17 -0500

    Friend component Created

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,12 @@
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+
+import Friend from "components/Friend";
+
+import { useState } from "react";
